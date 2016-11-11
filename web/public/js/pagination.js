24639ab7b82e230b4a0a9c60bfec2b0ecf52ff38
Phil <phil.pichet@gmail.com>
2016-11-11 19:23:35 +0100

    Gestion du refresh du render

--- web/public/js/pagination.js
+++ web/public/js/pagination.js
@@ -1,1 +1,13 @@
-var aElts = document.querySelectorAll('a.paginator');
+function paginationOnClick() {
+    $(".page").each(function(){
+        $(this).click(function(e) {
+            $('#contenu').load($(this).attr("href"), [], paginationOnClick );
+            console.log($(this).attr("href"));
+            e.preventDefault();
+        })
+    });
+}
+
+
+
+paginationOnClick();
