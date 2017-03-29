615f1d5c3581b85282e7c7912da17d1c89db47af
Phil <phil.pichet@gmail.com>
2017-03-29 16:07:13 +0200

    Modifie cross delete photo fig

--- web/public/js/chevron.js
+++ web/public/js/chevron.js
@@ -4,20 +4,10 @@ var aElts = document.querySelectorAll('.menu-drop');
 aElts.forEach(function (aElt) {
     // On ajoute un listener
     aElt.addEventListener('click', function (e) {
-        // On recupere la valeur de data-cible pour récuperer le div correspondant
-        var id = aElt.getAttribute('data-cible');
-        var divElt = document.getElementById(id);
-        // on verifie si le div contient la class 'in', puis on retire le chevron et on ajoute un nouveau chevron
-        if(divElt.classList.contains('in')){
-            var chevro
-            var chevronElt = aElt.querySelector('.chevron');
-            chevronElt.classList.remove('glyphicon-chevron-up');
-            chevronElt.classList.add('glyphicon-chevron-down');
-        }
-        if(divElt.classList.contains('in') ==  false){
-            var chevronElt = aElt.querySelector('.chevron');
-            chevronElt.classList.add('glyphicon-chevron-up');
-            chevronElt.classList.remove('glyphicon-chevron-down');
+        if($(this).hasClass('collapsed')){
+            $(this).children('h4').children('span').addClass("glyphicon-chevron-up").removeClass("glyphicon-chevron-down");
+        }else{
+            $(this).children('h4').children('span').addClass("glyphicon-chevron-down").removeClass("glyphicon-chevron-up");
         }
     })
 
